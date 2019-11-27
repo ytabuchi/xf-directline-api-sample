20ea786d231e504393bf3cfdf98e24b8a5634180
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-11-27 21:18:09 +0900

    add IBotService and BotService

---
+++ XFChatAppSample/XFChatAppSample/Services/BotService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector.DirectLine;
+
+namespace XFChatAppSample.Services
+{
+    public class BotService : IBotService
+    {
+        private static string BotUserName { get; } = "XFChatAppSample";
+
+        private IDirectLineClient DirectLineClient { get; }
+
+        public BotService(IDirectLineClient directLineClient)
+        {
+            this.DirectLineClient = directLineClient;
+        }
+
+        //public async Task<(IEnumerable<Activity> messages, string watermark)> GetMessagesAsync(string conversationId, string watermark)
+        //{
+        //    var r = await this.DirectLineClient.Conversations.GetActivitiesAsync(
+        //        conversationId,
+        //        watermark);
+        //    return (r.Activities, r.Watermark);
+        //}
+
+        //public async Task SendMessageAsync(string conversationId, string message)
+        //{
+        //    await this.DirectLineClient.Conversations.PostActivityAsync(
+        //        conversationId,
+        //        new Activity
+        //        {
+        //            From = new ChannelAccount(BotUserName),
+        //            Text = message,
+        //            Type = ActivityTypes.Message,
+        //        });
+        //}
+
+        //public async Task<string> StartConversationAsync()
+        //{
+        //    var conversation = await this.DirectLineClient.Conversations.StartConversationAsync();
+        //    return conversation.ConversationId;
+        //}
+    }
+}
